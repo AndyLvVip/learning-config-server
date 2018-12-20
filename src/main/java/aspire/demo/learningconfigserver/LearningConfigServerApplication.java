6d8b8b8436c87cca956694abcca7b3d418263409
andy.lv <lvhs@ucacc.com>
2018-12-20 13:30:50 +0800

    init

---
+++ src/main/java/aspire/demo/learningconfigserver/LearningConfigServerApplication.java
@@ -0,0 +1,14 @@
+package aspire.demo.learningconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningConfigServerApplication.class, args);
+	}
+
+}
+
